1a06f89c778c034fe0b3aeeda3ba29e222662a68
Javier Pérez Rubio <0611538986@uma.es>
2025-03-12 11:48:26 +0100

    Add method to Date.java

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -19,7 +19,10 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-
+  
+  public void methodOwner() {
+    System.out.println(day + " " + month + " " + year);
+  }
   /**
    * Retrieves the day component of this date.
    *
